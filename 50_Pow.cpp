584f74c93833fbd37ce5145970425a6ea105ae6b
jerryzj <jerry0715no14@gmail.com>
2020-06-21 19:13:19 +0800

    Add comments

--- 50_Pow.cpp
+++ 50_Pow.cpp
@@ -8,23 +8,26 @@ int main() {
     int n(-3);
 
     std::cout << "Ans: " << myPow(x, n) << std::endl;
-    return (0);
+    return 0;
 }
 
 double myPow(double x, int n) {
     double ans = compute(x, abs(n));
 
-    if (n < 0)
+    if (n < 0) {
         return 1.0 / ans;
-    else
+    } else {
         return ans;
+    }
 }
 
 inline double compute(double x, int n) {
     double ans(1.0);
     while (n != 0) {
-        if (n % 2 != 0)
+        // Reduce number of multiply by reduce n exponentially
+        if (n % 2 != 0) {
             ans *= x;
+        }
         x = x * x;
         n /= 2;
     }
