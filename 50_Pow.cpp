d6d6d09364bbf14d6d5dd3bdb67ac29fc6ea8361
jerryzj <jerry0715no14@gmail.com>
2020-06-21 19:07:46 +0800

    Add Pow

---
+++ 50_Pow.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+double myPow(double x, int n);
+inline double compute(double x, int n);
+
+int main() {
+    double x(2);
+    int n(-3);
+
+    std::cout << "Ans: " << myPow(x, n) << std::endl;
+    return (0);
+}
+
+double myPow(double x, int n) {
+    double ans = compute(x, abs(n));
+
+    if (n < 0)
+        return 1.0 / ans;
+    else
+        return ans;
+}
+
+inline double compute(double x, int n) {
+    double ans(1.0);
+    while (n != 0) {
+        if (n % 2 != 0)
+            ans *= x;
+        x = x * x;
+        n /= 2;
+    }
+    return ans;
+}
